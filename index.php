a4809c914ffc0a491af5559d57352f3cb5377a36
MariusNikolov <mariusnikolov4@gmail.com>
2021-01-30 19:35:53 +0200

    index.php
    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>#5 String Chain Replacements</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body style="background-color: powderblue">
+	<h1 style="text-align: center;">String Chain Replacements</h1>
+	<h2 style="text-align: center;">By: Marius Nikolov</h2>
+	<h3 style="text-align: center;">Given an array of strings, your task is to find the minimum amount of character changes required for the first character in each string to be equal to the last character of the string before it.</h3>
+	<form action="" method="">
+		<input type="submit" name="submit" value="Continue" style="margin-left: 45%">
+	</form>
+</body>
+</html>
